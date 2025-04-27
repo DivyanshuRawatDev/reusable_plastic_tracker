486fb9edaa9e4e598c8969faaecc5e064b2da549
DivyanshuRawatDev <divyanshu.rawat.dev@gmail.com>
2025-04-27 21:36:35 +0530

    Fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://reusable-plastic-tracker.vercel.app/", credentials: true }));
 //authentication
 app.use("/api/auth", userAuth);
 
